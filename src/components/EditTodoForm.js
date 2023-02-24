9d07182f8b3df9659ef68b34858df855a41a593d
Imbeshat <md.imbeshat@gmail.com>
2023-02-25 00:06:46 +0530

    added media queries for update button

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -12,7 +12,7 @@ const EditTodoForm = ({ editTodo, task }) => {
 	return (
 		<form className="todo-form" onSubmit={handleSubmit}>
 			<input type="text" className="todo-input" value={value} placeholder="Update your Todo" onChange={(e) => setValue(e.target.value)} />
-			<button type="submit" className="todo-btn-submit">
+			<button type="submit" className="todo-btn-update">
 				Update Todo
 			</button>
 		</form>
