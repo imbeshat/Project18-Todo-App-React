69b32f6982d387eaf9cd6df4949652597232a937
Imbeshat <md.imbeshat@gmail.com>
2023-03-12 21:33:06 +0530

    added delete all button and added multiple conditions and toast messages

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,26 +1,11 @@
 import React, { useState } from "react";
-import { Flip, toast } from "react-toastify";
 
 const TodoForm = ({ addTodo }) => {
 	const [value, setValue] = useState("");
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		if (value !== "") {
-			addTodo(value);
-		} else {
-			return toast("Todo can't be empty!", {
-				type: "error",
-				position: "bottom-center",
-				autoClose: 3500,
-				hideProgressBar: false,
-				closeOnClick: true,
-				draggable: true,
-				progress: undefined,
-				theme: "colored",
-				transition: Flip,
-			});
-		}
+		addTodo(value);
 		setValue("");
 	};
 
